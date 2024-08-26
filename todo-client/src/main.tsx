6eee42db939134d7ae682acf40fbbffd6425e766
Kaarle Järvinen <kaarle46@gmail.com>
2024-08-26 11:44:28 +0300

    Added auth0

--- todo-client/src/main.tsx
+++ todo-client/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-createRoot(document.getElementById('root')!).render(
+const domain = import.meta.env.VITE_AUTH_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH_ID;
+
+if (!domain || !clientId) {
+  throw new Error("Missing Auth0 environment variables");
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{ redirect_uri: window.location.origin }}
+    >
+      <App />
+    </Auth0Provider>
+  </StrictMode>
+);
