6d60a81f71e99352e25bbfaf3e5809d8a56991cb
Kaarle Järvinen <kaarle46@gmail.com>
2024-08-27 10:29:52 +0300

    Populated landing

--- todo-client/src/views/Landing.tsx
+++ todo-client/src/views/Landing.tsx
@@ -1,3 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+
 export default function Landing() {
-  return <div>Landing</div>;
+  const { isAuthenticated, loginWithRedirect } = useAuth0();
+  return (
+    <div>
+      <h1 className="my-6">Write up tasks you want to be done</h1>
+      <h3>ToDo-App on web</h3>
+      <Button className="text-foreground my-6" asChild={isAuthenticated}>
+        {isAuthenticated ? (
+          <a href="/todos">Get Started</a>
+        ) : (
+          <a onClick={() => loginWithRedirect()}>Get Started</a>
+        )}
+      </Button>
+    </div>
+  );
 }
