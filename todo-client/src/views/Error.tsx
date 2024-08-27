56b34240460b2bec11edcc456086f8bbf94bee81
Kaarle Järvinen <kaarle46@gmail.com>
2024-08-27 12:17:38 +0300

    Updated error view

--- todo-client/src/views/Error.tsx
+++ todo-client/src/views/Error.tsx
@@ -1,8 +1,23 @@
+import { useRouteError } from 'react-router-dom';
+
+interface RouteError {
+  statusText?: string;
+  message?: string;
+}
+
 export default function Error() {
+  const error = useRouteError();
+  console.error(error);
+
+  const typedError = error as RouteError;
+
   return (
     <div className="text-center text-primary">
-      <h1>404</h1>
-      <h2>The page you're looking for couldn't be found</h2>
+      <h1>Oops!</h1>
+      <h2>An unexpected error has occurred.</h2>
+      <p>
+        <i>{typedError.statusText || typedError.message}</i>
+      </p>
     </div>
   );
 }
