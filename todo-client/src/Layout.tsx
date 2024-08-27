565249a0a762741b77fcc43e1f6ceb880dc584e8
Kaarle Järvinen <kaarle46@gmail.com>
2024-08-27 09:57:14 +0300

    Added inter as sans font

--- todo-client/src/Layout.tsx
+++ todo-client/src/Layout.tsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="font-mono antialiased min-h-screen mt-20">
+    <div className="font-sans antialiased min-h-screen mt-20">
       <Navbar />
       <div className="md:px-28 py-5 md:py-24 flex flex-col justify-start items-start w-full">
         {children}
