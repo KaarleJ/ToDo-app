2239cd8d645978a6b145a52458d267f5dc8f743c
Kaarle Järvinen <kaarle46@gmail.com>
2024-08-28 14:06:49 +0300

    Changed securityfilter to use okta

--- todo-server/src/main/java/com/todo/todoserver/config/SecurityConfiguration.java
+++ todo-server/src/main/java/com/todo/todoserver/config/SecurityConfiguration.java
@@ -2,40 +2,46 @@ package com.todo.todoserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.MappedJwtClaimSetConverter;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import lombok.RequiredArgsConstructor;
+import static org.springframework.security.config.Customizer.withDefaults;
 
+import org.springframework.beans.factory.annotation.Value;
+import java.util.Collections;
+
+/**
+ * Configures our application with Spring Security to restrict access to our API
+ * endpoints.
+ */
 @Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfiguration {
 
-  private final JwtAuthenticationFilter jwtAuthFilter;
-  private final AuthenticationProvider authenticationProvider;
-  
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-        .csrf()
-        .disable()
-        .authorizeHttpRequests()
-        .requestMatchers("/api/auth/**", "/**")
-        .permitAll()
-        .anyRequest()
-        .authenticated()
-        .and()
-        .sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-        .authenticationProvider(authenticationProvider)
-        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+  @Value("${okta.oauth2.issuer}")
+  private String issuerUri;
 
-    return http.build();
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    /*
+     * This is where we configure the security required for our endpoints and setup
+     * our app to serve as
+     * an OAuth2 Resource Server, using JWT validation.
+     */
+    return http
+        .authorizeHttpRequests((authorize) -> authorize
+            .requestMatchers("/").permitAll()
+            .requestMatchers("/api/todos").hasAuthority("SCOPE_access:todos"))
+        .cors(withDefaults())
+        .oauth2ResourceServer(oauth2 -> oauth2
+            .jwt(withDefaults()))
+        .build();
   }
 }
