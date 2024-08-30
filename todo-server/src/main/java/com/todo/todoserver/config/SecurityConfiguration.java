e981ef517a4af89325b33a8542e6fa66f38b07d4
Kaarle Järvinen <kaarle46@gmail.com>
2024-08-30 13:36:32 +0300

    Fixed paths

--- todo-server/src/main/java/com/todo/todoserver/config/SecurityConfiguration.java
+++ todo-server/src/main/java/com/todo/todoserver/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.todo.todoserver.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,6 +14,7 @@ import org.springframework.beans.factory.annotation.Value;
  * Configures our application with Spring Security to restrict access to our API
  * endpoints.
  */
+@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
@@ -28,12 +30,13 @@ public class SecurityConfiguration {
      */
     return http
         .authorizeHttpRequests((authorize) -> authorize
-            .requestMatchers("/").permitAll()
+            .requestMatchers("/**", "/").permitAll()
             .requestMatchers("/api/user").authenticated()
             .requestMatchers("/api/todos").hasAuthority("SCOPE_access:todos"))
         .cors(withDefaults())
         .oauth2ResourceServer(oauth2 -> oauth2
             .jwt(withDefaults()))
         .build();
+
   }
 }
