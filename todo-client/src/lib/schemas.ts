9f992f3963853da5b7c7be032fbb8e9e6a5f7abf
Kaarle Järvinen <kaarle46@gmail.com>
2024-09-04 11:02:48 +0300

    Updated schema to check for string lengths

--- todo-client/src/lib/schemas.ts
+++ todo-client/src/lib/schemas.ts
@@ -5,7 +5,10 @@ export const todoFormSchema = z.object({
     .string()
     .min(3, "Title must be at least 3 characters")
     .max(255, "Title must be at most 255 characters"),
-  text: z.string(),
+  text: z
+    .string()
+    .min(3, "Text must be at least 3 characters")
+    .max(255, "Text must be at most 255 characters"),
   deadline: z.date({ message: "Invalid date" }),
   status: z.boolean({ message: "Invalid status" }),
 });
