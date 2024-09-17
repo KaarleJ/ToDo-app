17c350105753916f4dcf5dcc2f575f9a16dba41d
Kaarle Järvinen <kaarle46@gmail.com>
2024-09-17 11:36:48 +0300

    ...

--- todo-client/src/loaders.ts
+++ todo-client/src/loaders.ts
@@ -4,11 +4,5 @@ import apiClient from "./lib/apiClient";
 
 export const todosLoader = async () => {
   const todosResponse = await apiClient.get<Todo[]>(`${apiUrl}/api/todos`);
-  // Sort the response by date
-  /*
-  const todos = todosResponse.data.sort((a, b) => {
-    return new Date(a.deadline).getTime() - new Date(b.deadline).getTime();
-  });
-  */
   return todosResponse.data;
 };
