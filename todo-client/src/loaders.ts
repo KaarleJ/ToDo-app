84e6a375d0f62286eb184a0de9ac6821d53a9879
Kaarle Järvinen <kaarle46@gmail.com>
2024-09-17 11:35:05 +0300

    Finished landing page

--- todo-client/src/loaders.ts
+++ todo-client/src/loaders.ts
@@ -2,8 +2,13 @@ import { apiUrl } from "./lib/utils";
 import { Todo } from "./types";
 import apiClient from "./lib/apiClient";
 
-
 export const todosLoader = async () => {
-    const todosResponse = await apiClient.get<Todo[]>(`${apiUrl}/api/todos`);
-    return todosResponse.data;
+  const todosResponse = await apiClient.get<Todo[]>(`${apiUrl}/api/todos`);
+  // Sort the response by date
+  /*
+  const todos = todosResponse.data.sort((a, b) => {
+    return new Date(a.deadline).getTime() - new Date(b.deadline).getTime();
+  });
+  */
+  return todosResponse.data;
 };
--- todo-client/src/views/Landing.tsx
+++ todo-client/src/views/Landing.tsx
@@ -5,23 +5,35 @@ import Link from "@/components/Link";
 export default function Landing() {
   const { isAuthenticated, loginWithRedirect } = useAuth0();
   return (
-    <div className="w-full relative">
-      <h1 className="mt-28">Write up tasks you want to be done</h1>
-      <h3 className="my-6">ToDo-App on web</h3>
-      <p className="max-w-2xl my-4">
-        Your ultimate solution for managing tasks
-        efficiently and effectively. Juggle multiple projects
-        or simply keep track of daily chores, ToDo-App provides a seamless
-        and intuitive interface to help you stay organized. Get started now and
-        experience the ease of managing your tasks with ToDo-App!
-      </p>
-      <Button className="text-foreground my-6" asChild={isAuthenticated}>
-        {isAuthenticated ? (
-          <Link to="/todos">Get Started</Link>
-        ) : (
-          <a onClick={() => loginWithRedirect()}>Get Started</a>
-        )}
-      </Button>
+    <div className="h-full flex my-20 mx-4 md:mx-0">
+      <div className="w-full">
+        <h1>Write up tasks you want done</h1>
+        <h3 className="mb-6 text-muted-foreground">ToDo-App on web</h3>
+        <p className="max-w-2xl overflow-hidden text-lg my-12">
+          Your ultimate solution for managing tasks efficiently and effectively.
+          Juggle multiple projects or simply keep track of daily chores. Get
+          started now and experience the ease of managing your tasks with
+          ToDo-App!
+        </p>
+        <Button
+          className="text-foreground my-4 text-xl"
+          size="lg"
+          asChild={isAuthenticated}
+        >
+          {isAuthenticated ? (
+            <Link to="/todos">Get Started</Link>
+          ) : (
+            <a onClick={() => loginWithRedirect()}>Get Started</a>
+          )}
+        </Button>
+      </div>
+      <div className="w-full relative hidden md:flex">
+        <img
+          src="https://images.unsplash.com/photo-1661534855533-63d347a52303?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="organised desk"
+          className="w-full h-full object-cover object-right shadow-2xl shadow-primary dark:shadow-secondary-foreground rounded-sm"
+        />
+      </div>
     </div>
   );
 }
