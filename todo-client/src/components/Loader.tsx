6b42029c12d2da3e500b69a009a279a449d2794f
Kaarle Järvinen <kaarle46@gmail.com>
2024-09-19 09:17:06 +0300

    refactor

--- todo-client/src/components/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import { LoaderCircle as LoaderIcon } from "lucide-react";
-
-export default function Loader() {
-  return (
-    <div className="w-full h-full flex items-center justify-center">
-      <LoaderIcon size={64} className="animate-spin "/>
-    </div>
-  )
-}
