356e11a68e914dc9acba798426ffb2b784ac60ac
Kaarle Järvinen <kaarle46@gmail.com>
2024-09-26 10:31:59 +0300

    Edited mobileview

--- todo-client/src/views/Todos/TopMenu.tsx
+++ todo-client/src/views/Todos/TopMenu.tsx
@@ -9,54 +9,25 @@ import {
   MenubarSeparator,
   MenubarTrigger,
 } from "@/components/ui/menubar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Label } from "@/components/ui/label";
+import { Settings2 as Filter, Search } from "lucide-react";
 import AddTodoModal from "./AddTodoModal";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 
 export default function TopMenu() {
   const revalidator = useRevalidator();
   const [search, setSearch] = useSearchParams();
-
 
   function updateSearch(key: string, value: string) {
     const newParams = new URLSearchParams(search);
     newParams.set(key, value);
     setSearch(newParams);
     revalidator.revalidate();
-  };
-
-
-  function MenuBar() {
-    return (
-      <Menubar className="ml-4">
-        <MenubarMenu>
-          <MenubarTrigger className="w-16">Show</MenubarTrigger>
-          <MenubarContent>
-            <MenubarItem onClick={() => updateSearch("show", "all")}>
-              All
-            </MenubarItem>
-            <MenubarItem onClick={() => updateSearch("show", "finished")}>
-              Finished
-            </MenubarItem>
-            <MenubarItem onClick={() => updateSearch("show", "unfinished")}>
-              Unfinished
-            </MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger className="w-16">Sort</MenubarTrigger>
-          <MenubarContent>
-            <Label className="px-2">Deadline</Label>
-            <MenubarSeparator />
-            <MenubarItem onClick={() => updateSearch("sort", "asc")}>
-              Ascending
-            </MenubarItem>
-            <MenubarItem onClick={() => updateSearch("sort", "desc")}>
-              Descending
-            </MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-      </Menubar>
-    );
   }
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
@@ -65,13 +36,52 @@ export default function TopMenu() {
     revalidator.revalidate();
   }
 
-  return (
-    <div className="w-full border-b h-min px-4 py-2 flex items-center justify-between rounded-t-md">
-      <div className="w-52">
-        <h3>Todos</h3>
-        <p className="brightness-75 text-sm">Manage your tasks</p>
-      </div>
-      <div className="flex w-2/3 ml-4 justify-end gap-4 items-center">
+  function MenuBar() {
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger className="rounded-md hover:bg-accent border p-2">
+          <Filter className="text-foreground" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="p-0">
+          <Menubar className="border-none">
+            <MenubarMenu>
+              <MenubarTrigger className="w-16">Show</MenubarTrigger>
+              <MenubarContent>
+                <MenubarItem onClick={() => updateSearch("show", "all")}>
+                  All
+                </MenubarItem>
+                <MenubarItem onClick={() => updateSearch("show", "finished")}>
+                  Finished
+                </MenubarItem>
+                <MenubarItem onClick={() => updateSearch("show", "unfinished")}>
+                  Unfinished
+                </MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger className="w-16">Sort</MenubarTrigger>
+              <MenubarContent>
+                <Label className="px-2">Deadline</Label>
+                <MenubarSeparator />
+                <MenubarItem onClick={() => updateSearch("sort", "asc")}>
+                  Ascending
+                </MenubarItem>
+                <MenubarItem onClick={() => updateSearch("sort", "desc")}>
+                  Descending
+                </MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+          </Menubar>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
+  }
+
+  function SearchBar() {
+    const isDesktop = useMediaQuery("(min-width: 768px)");
+
+    if (isDesktop) {
+      return (
         <form onSubmit={handleSubmit} className="flex">
           <Input name="search" placeholder="filter task" />
           <Button
@@ -82,8 +92,32 @@ export default function TopMenu() {
             Search
           </Button>
         </form>
+      );
+    } else {
+      return (
+        <>
+        <Input name="search" placeholder="filter task" />
+        <Button className="border shadow-sm p-2" variant="secondary">
+          <Search size={26} />
+        </Button>
+        </>
+        
+      );
+    }
+  }
+
+  return (
+    <div className="w-full border-b h-min px-4 py-2 flex items-center justify-between rounded-t-md">
+      <div className="w-52">
+        <h3>Todos</h3>
+        <p className="brightness-75 text-sm">Manage your tasks</p>
+      </div>
+      <div className="flex w-2/3 ml-4 justify-end gap-2 items-center">
         <MenuBar />
-        <AddTodoModal />
+        <SearchBar />
+        <div className="ml-2">
+          <AddTodoModal />
+        </div>
       </div>
     </div>
   );
