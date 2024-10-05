70670433feb18f2ed198800a87d47fbb59ad4bf9
Kaarle Järvinen <kaarle46@gmail.com>
2024-10-05 10:47:07 +0300

    Missing auth throws appropriate exception

--- todo-server/src/main/java/com/todo/todoserver/service/JwtService.java
+++ todo-server/src/main/java/com/todo/todoserver/service/JwtService.java
@@ -1,12 +1,17 @@
 package com.todo.todoserver.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class JwtService {
   public String getIdFromToken(Authentication auth) {
+    if (auth == null) {
+      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized");
+    }
     Jwt jwt = (Jwt) auth.getPrincipal();
     String sub = jwt.getSubject();
     //Split the subject from | and get the last element
