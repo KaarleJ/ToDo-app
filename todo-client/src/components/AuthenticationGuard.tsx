ca8cbe70c26a357ba2e9009749b4c8f9989d1a8b
Kaarle Järvinen <kaarle46@gmail.com>
2024-10-10 16:51:29 +0300

    Better loader

--- todo-client/src/components/AuthenticationGuard.tsx
+++ todo-client/src/components/AuthenticationGuard.tsx
@@ -1,5 +1,6 @@
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import { ComponentType } from "react";
+import { LoaderCircle as Loader } from "lucide-react";
 
 export const AuthenticationGuard = ({
   component,
@@ -7,7 +8,7 @@ export const AuthenticationGuard = ({
   component: ComponentType<object>;
 }) => {
   const Component = withAuthenticationRequired(component, {
-    onRedirecting: () => <div>Loading...</div>,
+    onRedirecting: () => <div className="h-[30rem] w-full flex justify-center items-center"><Loader size={42} className="animate-spin" /></div>,
   });
 
   return <Component />;
